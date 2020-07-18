928a7647e46761c6696353459e3ee3109e239a57
AidanForsyth <AidanForsyth@users.noreply.github.com>
2020-07-18 23:00:26 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    string name = get_string("What is your name?\n");
-    printf("hello, %s\n", name);
+    string name = get_string("What is your name?\n");       //Creates a variable called "name" of type string. The variabe takes whatever string is inputed into the function get_string
+    printf("hello, %s\n", name);        //printf function displays the message to the human, the %s is a place holder for the variable input name. \n means inserting to a new line
 }
 
