21b873141cc4d751bc4b7dd80145a25187ebfbf2
AidanForsyth <AidanForsyth@users.noreply.github.com>
2020-07-19 14:19:19 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -1,6 +1,10 @@
+/*
+Aidan Forsyth
+Creating mario blocks in C
+CS50x
+*/
 #include <stdio.h>
 #include <cs50.h>
-
 
 
 int main(void)
@@ -10,10 +14,10 @@ int main(void)
     {
         n = get_int("Height: ");
     }
-    while ((n < 1) || (n > 8));
+    while ((n < 1) || (n > 8));  //condition that will reject heights not included in the specified range
     for (int i = 1; i <= n; ++i, k = 0)
     {
-       for (int spaces = (n-i); spaces > 0; spaces--)
+       for (int spaces = (n-i); spaces > 0; spaces--) //adds the spaces for the left block
        {
            printf(" ");
        }
@@ -23,14 +27,14 @@ int main(void)
             printf("#");
         }
         
-        printf("  ");
+        printf("  "); //this creates the gap
         
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= i; j++) //creates the second block
         {
             printf("#");
         }
         
         printf("\n");
     }
-    return 0;
+    return 0; //indicates sucess and completion of function
 }
