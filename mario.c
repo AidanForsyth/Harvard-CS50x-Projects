c31fb121acc2825f81bf0c37bc01dc12304040f8
AidanForsyth <AidanForsyth@users.noreply.github.com>
2020-07-19 14:04:42 +0000

    automated commit by check50 [check50=True]

---
+++ mario.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+
+int main(void)
+{
+    int n, k = 0;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while ((n < 1) || (n > 8));
+    for (int i = 1; i <= n; ++i, k = 0)
+    {
+       for (int spaces = (n-i); spaces >= 0; spaces--)
+       {
+           printf(" ");
+       }
+       
+       for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        
+        printf("  ");
+        
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        
+        printf("\n");
+    }
+    return 0;
+}
