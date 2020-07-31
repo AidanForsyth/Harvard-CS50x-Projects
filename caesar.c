28b401c87169c16dceaf6f697def54c4ab6851ce
AidanForsyth <AidanForsyth@users.noreply.github.com>
2020-07-31 01:12:13 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, string argv[])
+int main(int argc, string argv[]) //set up for command line arguments
 {
     if (argc != 2)
     {
@@ -12,31 +12,31 @@ int main(int argc, string argv[])
         return 1;
     }
     
-    for (int i = 0, n = strlen(argv[1]); i < n; i++)
+    for (int i = 0, n = strlen(argv[1]); i < n; i++) //cycles through each inputed "key" character 
     {
-        if (isdigit(argv[1][i]) == 0)
+        if (isdigit(argv[1][i]) == 0) //this will cycle through the inputed key to check if all characters are digits (0-9)
         {
             printf("Usage: ./ceaser key\n");
             return 1; 
         }
     }
     //printf("Success\n");
-    int key = atoi(argv[1]);
+    int key = atoi(argv[1]); //atoi converts the string into a integer
     printf("%i\n", key);
-    string plaintext = get_string("Plaintext: ");
+    string plaintext = get_string("Plaintext: "); //gets the users input for what they want the key to be 
     
     printf("ciphertext: ");
-    for (int j = 0, p = strlen(plaintext); j < p; j++)
+    for (int j = 0, p = strlen(plaintext); j < p; j++) // this is a way to cycle through each individual character in the "plaintext"
     {
-        if (isupper(plaintext[j]))
+        if (isupper(plaintext[j])) //checks if character is upper case 
         {
             printf("%c", ((((plaintext[j] - 65) + key) % 26) + 65));
         }
-        else if (islower(plaintext[j]))
+        else if (islower(plaintext[j])) //checks if character is lower case
         {
             printf("%c", ((((plaintext[j] - 97) + key) % 26) + 97));
         }
-        else 
+        else //accounts for any special characters (coma, spaces, etc.)
         {
             printf("%c", plaintext[j]);
         }
